156df13cea452f3fa566cdc8bb78643a138c5cf6
programmergeek <programergeek@gmail.com>
2021-07-28 16:38:52 +0200

    got elements created inital state of the menu and sub menu

--- index.js
+++ index.js
@@ -2,8 +2,15 @@
 const button = document.getElementById("hover-drop-menu")
 const menuItems = document.getElementById("hover-menu-items")
 
+//get elements (sub menu)
+const subMenuButton = document.getElementById("hover-drop-submenu")
+const menu = document.getElementById("hover-menu-items-two")
+const subMenu = document.getElementById("hover-submenu-items")
+
 //initial state
 menuItems.style.display = "none"
+menu.style.display = "none"
+subMenu.style.display = "none"
 
 //event handlers
 button.addEventListener("mouseover", function(){
